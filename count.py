08eeefe79650ede3faa773ed62ea7e8fe449289e
CrickWu <crickwu@gmail.com>
2014-05-19 21:19:00 +0800

    add count.py

---
+++ count.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import sys
+from string import *
+
+count = 0
+if len(sys.argv) > 2:
+	f = sys.argv[1]
+else:
+	f = '\t1'
+for line in sys.stdin:
+	count += line.count(f)
+print count
+#print sys.argv[1].count(sys.argv[2])
+
